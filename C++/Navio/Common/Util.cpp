fa7a3caf1be32574708ae3e81b72317d64ee59a3
dohi-H5 <masa0u0masa1215@gmail.com>
2023-07-26 23:21:46 +0900

    edit: Util.cpp
    decodeBinary32を修正

--- C++/Navio/Common/Util.cpp
+++ C++/Navio/Common/Util.cpp
@@ -5,10 +5,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <cmath>
+#include <cinttypes>
 
 #include "Util.h"
 
 #define SCRIPT_PATH "../../../check_apm.sh"
+
+using namespace std;
 
 int write_file(const char* path, const char* fmt, ...)
 {
@@ -80,19 +83,27 @@ int get_navio_version()
   return version;
 }
 
-float decodeBinary32(unsigned int bin)
+// float decodeBinary32(uint32_t bin)
+// {
+//   const int sign = bin >> 31 ? -1 : 1;
+//   const int exponent = ((bin & 0x7f800000) >> 23) - 0x7F;
+//   const uint32_t fraction = bin & 0x007fffff;
+
+//   uint32_t pow2 = 1 << 23;
+//   float fraction_decoded = 0.;
+//   for (uint32_t i = 0; i < 23; ++i)
+//   {
+//     fraction_decoded += static_cast<float>((fraction >> i) & 1) / pow2;
+//     pow2 = pow2 >> 1;
+//   }
+
+//   return sign * (1 + fraction_decoded) * pow(2, exponent);
+// }
+
+float decodeBinary32(uint32_t bin)
 {
-  int sign = bin >> 31 ? -1 : 1;
-  int exponent = ((bin & 0x7f800000) >> 23) - 0x7F;
-  unsigned int fraction = bin & 0x007fffff;
-
-  unsigned int pow2 = 1 << 23;
-  float fraction_decoded = 0.;
-  for (unsigned int i = 0; i < 23; ++i)
-  {
-    fraction_decoded += static_cast<float>((fraction >> i) & 1) / pow2;
-    pow2 = pow2 >> 1;
-  }
-
-  return sign * (1 + fraction_decoded) * std::pow(2, exponent);
+  const int sign = bin >> 31 ? -1 : 1;
+  const int exponent = (bin >> 23) & 0xFF;
+  const int mantissa = (exponent == 0) ? (bin & 0x7FFFFF) << 1 : (bin & 0x7FFFFF) | 0x800000;
+  return sign * mantissa * pow(2, exponent - 150);
 }
