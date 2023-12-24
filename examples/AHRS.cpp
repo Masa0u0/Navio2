d49047017462d70d2f03d31c4fcc062032087864
Masa0u0 <masa0u0masa1215@gmail.com>
2023-12-24 15:20:43 +0900

    static_castを使用

--- examples/AHRS.cpp
+++ examples/AHRS.cpp
@@ -283,9 +283,9 @@ float AHRS::invSqrt(float x)
 {
   float halfx = 0.5f * x;
   float y = x;
-  long i = *(long*)&y;
+  long i = static_cast<long>(y);
   i = 0x5f3759df - (i >> 1);
-  y = *(float*)&i;
+  y = static_cast<float>(i);
   y = y * (1.5f - (halfx * y * y));
   return y;
 }
